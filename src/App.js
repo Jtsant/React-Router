3a24012a2fa49d66f06d33f4d61dfe0b07295180
Jtsant <jtsant92@hotmail.com>
2022-08-16 16:02:26 +0300

    Dynamic Routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route } from "react-router-dom";
 
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
 import MainHeader from "./components/MainHeader";
 
 function App() {
@@ -9,20 +10,22 @@ function App() {
     <div>
       <MainHeader />
       <main>
-       <Route path="/welcome">
-        <Welcome />
-      </Route>
-      <Route path="/products">
-        <Products />
-      </Route> 
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+        <Route path="/product-detail:ProductId">
+          <ProductDetail />
+        </Route>
       </main>
-      
     </div>
   );
 }
 
 export default App;
 
-
 // our-domain.com/welcome => Welcome Component
 // our-domain.com/products => Products Component
+// our-domain.com/product-detail/a-book(anything)
